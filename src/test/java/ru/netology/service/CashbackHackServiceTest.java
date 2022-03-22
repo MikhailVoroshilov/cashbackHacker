3e5c91fadc05a138c5dab6a3d06edee2e141f070
Mikhail Voroshilov <m.s.voroshilovv@gmail.com>
2022-03-22 13:21:58 +0800

    create branch JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -12,7 +12,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(0);
         int expected = 1000;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -20,7 +20,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(2000);
         int expected = 1000;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -28,6 +28,13 @@ public class CashbackHackServiceTest {
         int actual = service.remain(10000);
         int expected = 1000;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldRemainBoundaryNotComment() {
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(expected, actual);
     }
 }
